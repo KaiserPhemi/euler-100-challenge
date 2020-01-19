71ad3dbba89ed5a1f39526689d2af43e1bd1da2a
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-01-19 02:14:04 +0100

    ch(completed)
    - add test cases
    - fix assertion error

--- 01_3_&_5_multiples/multiplesOf3And5.js
+++ 01_3_&_5_multiples/multiplesOf3And5.js
@@ -1,7 +1,30 @@
+const arrSum = (acc, currentValue) => acc + currentValue;
+
+/**
+ * @desc 
+ */
 module.exports = numberInput => { 
-  // check if it's a number
-  // check for multiples of 3 and 5
-  // store in an array
-  // add everything in that array
+  // checks if input is number
+  if (typeof numberInput != 'number') {
+    return `Input must be a number!`;
+  }
+
+  let numArray = [];
+  let numCounter=1
+
+  //checks for multiples of 3 & 5
+  while (numCounter < numberInput) {
+    if (numCounter % 15 === 0 ||
+      numCounter % 5 === 0 ||
+      numCounter % 3 === 0) {
+      numArray.push(numCounter);
+    }
+    numCounter++;
+  }
+
+  // sum all array element
+  let result = numArray.reduce(arrSum, 0);
+
   // return the result
+  return result;
 };
