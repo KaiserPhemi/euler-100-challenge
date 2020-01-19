01645fd244d3e92557b4a4419ac8eb41384c584b
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-01-19 05:27:47 +0100

    ft(solve)
    - remove recursive function

--- 02_even-fibbonacci-number/evenFibonacciNum.js
+++ 02_even-fibbonacci-number/evenFibonacciNum.js
@@ -1,4 +1,29 @@
 /**
+ * @desc adds number
+ * @param {number} acc 
+ * @param {number} currentValue 
+ */
+const numSum = (acc, currentValue) => acc + currentValue;
+
+/**
+ * @desc using while loop to generate fibonacci sequence
+ * @param {number} num 
+ */
+const fibonacciSeqArr = inputNum => {
+  let fibSeqArray = [];
+  let numCounter = 1;
+  while (numCounter <= inputNum) {
+    if (numCounter <= 2) {
+      fibSeqArray.push(numCounter);
+    } else {
+      fibSeqArray.push(fibSeqArray[numCounter - 2] + fibSeqArray[numCounter - 3]);
+    }
+    numCounter++;
+  }
+  return fibSeqArray
+}
+
+/**
  * @desc generates a fibonacci sequence in an array
  * @param {number} numInput
  */
@@ -7,22 +32,27 @@ const genFibNums = numInput => {
   if (typeof numInput != 'number') {
     return `Input must be a number!`;
   }
-  let fibSequence = [];
-
+  
   // checks if input is greater than 0
   if (numInput <= 0) {
-    return `Number must be greater than 0;`
+    return `Number must be greater than zero(0);`
   }
+  return fibonacciSeqArr(numInput);
+}
 
-
-  return fibSequence;
+/**
+ * @desc returns an array of even numbers
+ * @param {array} numArr 
+ */
+const getEvenNums = numArr => {
+  return numArr.filter(numElement => numElement % 2 === 0);
 }
 
 /**
  * @desc
  */
 module.exports = numInput => {
-  
-
-  return true;
-}
+  const numArr = genFibNums(numInput);
+  const evenNumberArray = getEvenNums(numArr);
+  return evenNumberArray.reduce(numSum, 0);
+};
--- 02_even-fibbonacci-number/evenFibonacciNum.test.js
+++ 02_even-fibbonacci-number/evenFibonacciNum.test.js
@@ -2,9 +2,8 @@
 const assert = require('assert');
 const evenFibNum = require('./evenFibonacciNum');
 
-
+// test cases
 assert.deepStrictEqual(evenFibNum(10), 44)
-  assert.deepStrictEqual(evenFibNum(18), 3382)
+assert.deepStrictEqual(evenFibNum(18), 3382)
 assert.deepStrictEqual(evenFibNum(23), 60696)
 assert.deepStrictEqual(evenFibNum(43), 350704366)
-assert.deepStrictEqual(evenFibNum(10), 44)
