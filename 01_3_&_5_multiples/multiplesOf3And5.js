01e4d275877dd502fe4d7c9351376a93ea04bcba
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-01-19 01:08:21 +0100

    project-initialization
    - add first challenge folder & file

---
+++ 01_3_&_5_multiples/multiplesOf3And5.js
@@ -0,0 +1,7 @@
+module.exports = numberInput => { 
+  // check if it's a number
+  // check for multiples of 3 and 5
+  // store in an array
+  // add everything in that array
+  // return the result
+};
