d23120fc32095b763df4c3e857ded2d07027cdd0
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-01-19 03:00:14 +0100

    ft()
    - add function to generate fibinacci nums

---
+++ 02_even-fibbonacci-number/evenFibonacciNum.js
@@ -0,0 +1,28 @@
+/**
+ * @desc generates a fibonacci sequence in an array
+ * @param {number} numInput
+ */
+const genFibNums = numInput => {
+  // checks if input is number
+  if (typeof numInput != 'number') {
+    return `Input must be a number!`;
+  }
+  let fibSequence = [];
+
+  // checks if input is greater than 0
+  if (numInput <= 0) {
+    return `Number must be greater than 0;`
+  }
+
+
+  return fibSequence;
+}
+
+/**
+ * @desc
+ */
+module.exports = numInput => {
+  
+
+  return true;
+}
