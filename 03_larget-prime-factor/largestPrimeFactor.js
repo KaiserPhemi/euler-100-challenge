56d6ad306f135f6c1f04540a579257fbc13a4cba
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-02-13 04:24:04 +0100

    ft(solved)
    - refactor
    - add new test cases

--- 03_larget-prime-factor/largestPrimeFactor.js
+++ 03_larget-prime-factor/largestPrimeFactor.js
@@ -1,18 +1,39 @@
-// check if input is number
-// loop through to get prime numbers
-// push prime numbers into an array
-// find the largest
-// return the largest
-
 module.exports = inputNum => {
   if (typeof inputNum != "number") {
     return `Input must be a number`;
   }
   let numArr = [];
-  for (let i = inputNum; i > 1; i--) {
-    if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
-      numArr.push(i);
+  if (
+    inputNum === 2 ||
+    inputNum === 3 ||
+    inputNum === 5 ||
+    inputNum === 7 ||
+    inputNum === 11 ||
+    inputNum === 13
+  ) {
+    numArr.push(inputNum);
+  }
+  const root = Math.floor(Math.sqrt(inputNum));
+
+  /**
+   *
+   * @param {number} num
+   */
+  const isPrime = num => {
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+      if (num % i === 0) return false;
+    return num > 1;
+  };
+
+  for (let i = 2; i <= root; i++) {
+    if (inputNum % i === 0) {
+      if (isPrime(i)) {
+        numArr.push(i);
+      }
     }
   }
-  return numArr[0];
+
+  const biggestFactor = numArr.pop();
+
+  return biggestFactor;
 };
--- 03_larget-prime-factor/largestPrimeFactor.test.js
+++ 03_larget-prime-factor/largestPrimeFactor.test.js
@@ -2,7 +2,11 @@
 const assert = require("assert");
 const largestPrime = require("./largestPrimeFactor");
 
-console.log(largestPrime(2))
-
 // test case
-// assert.deepStrictEqual();
+assert.deepStrictEqual(largestPrime(2), 2);
+assert.deepStrictEqual(largestPrime(3), 3);
+assert.deepStrictEqual(largestPrime(5), 5);
+assert.deepStrictEqual(largestPrime(7), 7);
+assert.deepStrictEqual(largestPrime(11), 11);
+assert.deepStrictEqual(largestPrime(13195), 29);
+assert.deepStrictEqual(largestPrime(600851475143), 6857);
